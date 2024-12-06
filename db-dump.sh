05219cbec94e524058da2276246d8355e9a7b905
akiya <autumnsky64@gmail.com>
2024-12-06 17:08:37 +0900

    Replace Gitea with Forgejo

--- db-dump.sh
+++ db-dump.sh
@@ -8,10 +8,10 @@ docker exec -u postgres akkoma-db pg_dump -U ${DB_USER} -d ${DB_NAME} --format=c
 docker cp akkoma-db:/tmp/akkoma.pgdump /home/akiya/sites/dump/akkoma_${TODAY}.pgdump
 docker exec akkoma-db rm /tmp/akkoma.pgdump
 
-# gitea
-source /home/akiya/sites/gitea/.env
-docker exec -u postgres gitea-db-1 pg_dump -U ${GITEA_DB_USER} -d ${GITEA_DB_NAME} --format=custom -f /tmp/gitea.pgdump
-docker cp gitea-db-1:/tmp/gitea.pgdump /home/akiya/sites/dump/gitea_${TODAY}.pgdump
-docker exec gitea-db-1 rm /tmp/gitea.pgdump
+# forgejo
+source /home/akiya/sites/forgejo/.env
+docker exec -u postgres forgejo-db pg_dump -U ${FORGEJO_DB_USER} -d ${FORGEJO_DB_NAME} --format=custom -f /tmp/forgejo.pgdump
+docker cp forgejo-db:/tmp/forgejo.pgdump /home/akiya/sites/dump/forgejo_${TODAY}.pgdump
+docker exec forgejo-db rm /tmp/forgejo.pgdump
 
 # WordPress
